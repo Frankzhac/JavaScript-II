340a947b150595399e2631f871b9f237196d4514
Frankzhac <Frank.hemle@gmail.com>
2019-04-09 20:08:16 -0400

    updated closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,8 +1,18 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function ditBonjour() {
+  const name = "Elohim";
+  function greet() {
+    console.log("Bonjour " + name)
+    }
+    greet();
+  }
+console.log(ditBonjour());
+
+// Another method to write closure
 function daGod() {
   const king = "Elohim";
-  console.log(`Hello from above! How are you ${Elohim}`)
+  console.log(`Hello from Earth! How are you ${Elohim}`)
 
   function planet() {
     const name = "Yashua";
@@ -10,20 +20,26 @@ function daGod() {
   }
   planet();
 }
+console.log(daGod());
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
+// const counter = () => {
+//   // Return a function that when invoked increments and returns a counter variable.
+// };
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-};
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+  let count = 0;
 
-// ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
-};
+  return () => (++count);
+  };
+// // Example usage: const newCounter = counter();
+// // newCounter(); // 1
+// // newCounter(); // 2
+//
+// // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
+// const counterFactory = () => {
+//   // Return an object that has two methods called `increment` and `decrement`.
+//   // `increment` should increment a counter variable in closure scope and return it.
+//   // `decrement` should decrement the counter variable and return it.
+// };
